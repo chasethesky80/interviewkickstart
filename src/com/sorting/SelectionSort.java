936148a188a2391f98ac7e2bfb8bd8d4b6f3afec
chasethesky79 <seshadri.bharath@yahoo.com>
2025-02-15 17:52:19 -0600

    Implemented selection sort

---
+++ src/com/sorting/SelectionSort.java
@@ -0,0 +1,28 @@
+package com.sorting;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        System.out.println("SORTED ELEMENTS USING SELECTION SORT "+ performSelectionSort(Arrays.asList(2, 5, 3, 7, 1, 6)));
+    }
+
+    private static List<Integer> performSelectionSort(final List<Integer> input) {
+        for (int i = 0; i < input.size(); i++) {
+            int currentMin = input.get(i);
+            int currentMinIndex = i;
+            for (int j = i+1; j < input.size(); j++) {
+                if (input.get(j).compareTo(currentMin) < 0) {
+                    currentMinIndex = j;
+                    currentMin = input.get(j);
+                }
+            }
+            int temp = input.get(i);
+            input.set(i, input.get(currentMinIndex));
+            input.set(currentMinIndex, temp);
+        }
+        return input;
+    }
+}
