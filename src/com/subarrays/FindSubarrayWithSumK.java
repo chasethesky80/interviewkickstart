d0e5bed5c455b3feddf07fd2d77b35b635a4ec17
chasethesky79 <seshadri.bharath@yahoo.com>
2025-02-22 21:49:38 -0600

    Implemented subarrays with target sum

---
+++ src/com/subarrays/FindSubarrayWithSumK.java
@@ -0,0 +1,29 @@
+package com.subarrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindSubarrayWithSumK {
+
+    public static void main(String[] args) {
+        System.out.println("FIND NUMBER OF SUBARRAYS WITH SUM OF "+findSubArrayCountWithSumS(Arrays.asList(3, 7, 5, 8, 2, 10), 3, 15));
+    }
+
+    private static int findSubArrayCountWithSumS(final List<Integer> input, final int K, final int targetSum) {
+        int count = 0, sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum = sum + input.get(i);
+            if (sum == targetSum) {
+                count++;
+            }
+        }
+        for (int i = 1; i <= input.size() - K; i++) {
+            int j = i + K -1;
+            sum = sum - input.get(i-1) + input.get(j);
+            if (sum == targetSum) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
