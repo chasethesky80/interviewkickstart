113c0ad5d2b113a0c15bb6cb6f2c898f17c49698
chasethesky79 <seshadri.bharath@yahoo.com>
2025-03-17 19:48:39 +0530

    Perform insertion sort

---
+++ src/com/sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+package com.sorting;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        System.out.println("PERFORM INSERTION SORT "+ performInsertionSort(Arrays.asList(6, 5, 1, 8, 3, 2)));
+    }
+
+    private static List<Integer> performInsertionSort(final List<Integer> input) {
+        for (int i = 0; i < input.size(); i++) {
+            int temp = input.get(i);
+            int red = i - 1;
+            while (red >= 0 && input.get(red) > temp) {
+                input.set(red + 1, input.get(red));
+                red--;
+            }
+            input.set(red + 1, temp);
+        }
+        return input;
+    }
+}
