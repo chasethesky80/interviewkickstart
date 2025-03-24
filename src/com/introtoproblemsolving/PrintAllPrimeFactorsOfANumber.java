8cdf65914af6c7da816830386cd7828af7c6a5b1
chasethesky79 <seshadri.bharath@yahoo.com>
2025-03-24 15:35:05 +0530

    Implement prime factors of a number

---
+++ src/com/introtoproblemsolving/PrintAllPrimeFactorsOfANumber.java
@@ -0,0 +1,28 @@
+package com.introtoproblemsolving;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintAllPrimeFactorsOfANumber {
+
+    public static void main(String[] args) {
+        System.out.println("PRIME FACTORS OF A NUMBER "+ primeFactorsOfNumber(24));
+        System.out.println("PRIME FACTORS OF A NUMBER "+ primeFactorsOfNumber(25434));
+    }
+
+    private static List<Integer> primeFactorsOfNumber(int N) {
+        final List<Integer> primeFactors = new ArrayList<>();
+        int i = 2;
+        while (N % i == 0) {
+            primeFactors.add(i);
+            N = N / i;
+        }
+        for (int j = 3; j <= N; j = j + 2) {
+            while (N % j == 0) {
+                primeFactors.add(j);
+                N = N / j;
+            }
+        }
+        return primeFactors;
+    }
+}
