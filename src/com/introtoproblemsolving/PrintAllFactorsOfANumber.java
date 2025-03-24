2de23872e056d3e5e6ced42ea994124b198e69dd
chasethesky79 <seshadri.bharath@yahoo.com>
2025-03-24 14:16:07 +0530

    Implemented optimized print all factors method

--- src/com/introtoproblemsolving/PrintAllFactorsOfANumber.java
+++ src/com/introtoproblemsolving/PrintAllFactorsOfANumber.java
@@ -7,6 +7,10 @@ public class PrintAllFactorsOfANumber {
         printAllFactors(100);
     }
 
+    /**
+     * FIND ALL FACTORS OF NUMBER OPTIMIZED, TC = O(SQRT(N)) and SC = Constant
+     * @param N
+     */
     private static void printAllFactors(int N) {
         for (int i = 1; i*i <= N; i++) {
             if (N % i == 0) {
