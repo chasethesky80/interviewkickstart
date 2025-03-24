a452d8a326f06bd3c4a6eb1932cd492abcf4a3e4
chasethesky79 <seshadri.bharath@yahoo.com>
2025-03-24 16:08:27 +0530

    Implemented cube sum for a number

---
+++ src/com/introtoproblemsolving/Find2NumbersWhoseCubeSumIsN.java
@@ -0,0 +1,20 @@
+package com.introtoproblemsolving;
+
+public class Find2NumbersWhoseCubeSumIsN {
+
+    public static void main(String[] args) {
+        System.out.println("SUM OF CUBE OF NUMBERS FOR \n");
+        cubeSum(28);
+    }
+
+    private static void cubeSum(final Integer N) {
+        for (int i = 1; i <= N; i++) {
+            int a = i * i * i;
+            int diff = N - a;
+            int cbRt = (int) Math.cbrt(diff);
+            if (cbRt * cbRt * cbRt == diff) {
+                System.out.print("CUBE SUM "+ a +" "+diff);
+            }
+        }
+    }
+}
