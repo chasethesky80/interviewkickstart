e75d96f0d697d45a39a31d1fa98276e67194d3e2
chasethesky79 <seshadri.bharath@yahoo.com>
2025-03-27 17:15:30 +0530

    Implemented solution to find pair with maximum product

--- src/com/introtoproblemsolving/GivenAnArrayFindPairWithHighestProduct.java
+++ src/com/introtoproblemsolving/GivenAnArrayFindPairWithHighestProduct.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class GivenAnArrayFindPairWithHighestProduct {
 
      public static void main(String[] args) {
-          System.out.println("PAIR WITH HIGHEST PRODUCT "+ findPairWithHighestProduct(Arrays.asList(-1, -3, -4, 2, 0, -5)));
+          System.out.println("PAIR WITH HIGHEST PRODUCT "+ findPairWithHighestProductBruteForce(Arrays.asList(-1, -3, -4, 2, 0, -5)));
      }
 
 
-     private static Pair findPairWithHighestProduct(final List<Integer> input) {
+     private static Pair findPairWithHighestProductBruteForce(final List<Integer> input) {
           Integer maxProduct = Integer.MIN_VALUE, left = -1, right = -1;
           for (int i = 0; i < input.size(); i++) {
                for (int j = i + 1; j < input.size(); j++) {
