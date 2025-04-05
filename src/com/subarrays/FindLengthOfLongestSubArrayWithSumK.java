5774f935339db10c4c958c5e820d34fbaba30b07
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-05 15:36:56 +0530

    Implemented solution for longest subarray with sum K

---
+++ src/com/subarrays/FindLengthOfLongestSubArrayWithSumK.java
@@ -0,0 +1,33 @@
+package com.subarrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindLengthOfLongestSubArrayWithSumK {
+
+    public static void main(String[] args) {
+        System.out.println("LONGEST SUBARRAY WITH SUM "+ longestSubArrayWithSumK(Arrays.asList(2, 0, 0, 3), 3));
+    }
+
+    private static Integer longestSubArrayWithSumK(final List<Integer> input, final Integer K) {
+        int maxLength = Integer.MIN_VALUE, sum = 0;
+        final Map<Integer, Integer> prefixSumMap = new HashMap<>();
+        for (int i = 0; i < input.size(); i++) {
+            sum = sum + input.get(i);
+            if (sum == K) {
+                maxLength = Math.max(maxLength, i+1);
+            } else {
+                int difference = sum - K;
+                if (prefixSumMap.containsKey(difference)) {
+                    maxLength = Math.max(maxLength, i - prefixSumMap.get(difference));
+                }
+                if (!prefixSumMap.containsKey(sum)) {
+                    prefixSumMap.put(sum, i);
+                }
+            }
+        }
+        return maxLength;
+    }
+}
