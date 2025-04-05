6d43ef2c7b8afd953da0f67479cd62f0ee3b72db
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-05 18:32:53 +0530

    Implemented longest subarray with sum K for positives and zeros only

--- src/com/subarrays/FindLengthOfLongestSubArrayWithSumK.java
+++ src/com/subarrays/FindLengthOfLongestSubArrayWithSumK.java
@@ -9,6 +9,11 @@ public class FindLengthOfLongestSubArrayWithSumK {
 
     public static void main(String[] args) {
         System.out.println("LONGEST SUBARRAY WITH SUM "+ longestSubArrayWithSumK(Arrays.asList(2, 0, 0, 3), 3));
+        System.out.println("LONGEST SUBARRAY WITH SUM USING 2 POINTER APPROACH "+ longestSubArrayWithSumKTwoPointerApproach(Arrays.asList(2, 0, 0, 3), 3));
+        System.out.println("LONGEST SUBARRAY WITH SUM USING 2 POINTER APPROACH "+ longestSubArrayWithSumKTwoPointerApproach(Arrays.asList(
+                2, 0, 0, 3, 1, 2, 0, 0, 0), 3));
+        System.out.println("LONGEST SUBARRAY WITH SUM USING 2 POINTER APPROACH "+ longestSubArrayWithSumKTwoPointerApproach(Arrays.asList(
+                2, 0, 0, 3, 1, 2, 0, 0), 3));
     }
 
     private static Integer longestSubArrayWithSumK(final List<Integer> input, final Integer K) {
@@ -26,6 +31,32 @@ public class FindLengthOfLongestSubArrayWithSumK {
                 if (!prefixSumMap.containsKey(sum)) {
                     prefixSumMap.put(sum, i);
                 }
+            }
+        }
+        return maxLength;
+    }
+
+    /**
+     * 2 POINTER APPROACH IF THE SUBARRAY ONLY CONTAINS POSITIVES AND ZEROS
+     */
+    private static Integer longestSubArrayWithSumKTwoPointerApproach(final List<Integer> input, final Integer K) {
+        int i = 0, j = 0, sum = 0, maxLength = Integer.MIN_VALUE;
+        sum = sum + input.get(0);
+        while (j < input.size()) {
+            if (sum == K) {
+                maxLength = Math.max(maxLength, j-i + 1);
+                j++;
+                if (j < input.size()) {
+                    sum = sum + input.get(j);
+                }
+            } else if (sum < K) {
+                j++;
+                if (j < input.size()) {
+                    sum = sum + input.get(j);
+                }
+            } else {
+                sum = sum - input.get(i);
+                i++;
             }
         }
         return maxLength;
