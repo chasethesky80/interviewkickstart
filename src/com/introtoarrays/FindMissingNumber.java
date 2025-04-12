0ba854ae7a49f3dfc4f1b69926a3c3d4f9a01bed
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-12 18:45:49 +0530

    Implemented finding the missing number

--- src/com/introtoarrays/FindMissingNumber.java
+++ src/com/introtoarrays/FindMissingNumber.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Given an array of size N, one number is missing find the number
+ * Given an array of size N and the array contains numbers from 0 to N-1, one number is missing find the number
  */
 public class FindMissingNumber {
 
     public static void main(String[] args) {
-        System.out.println("MISSING NUMBER "+ findMissingNumber(Arrays.asList(5, 4, 2 , 1)));
+        System.out.println("MISSING NUMBER "+ findMissingNumber(Arrays.asList(5, 4, 2, 1)));
+        System.out.println("MISSING NUMBER "+ findMissingNumber(Arrays.asList(0, 1, 3, 4)));
+        System.out.println("MISSING NUMBER "+ findMissingNumber(Arrays.asList(0, 1, 2, 3, 4)));
         System.out.println("MISSING NUMBER "+ findMissingNumber(Arrays.asList(9, 6, 4, 2, 3, 5, 7, 0, 1)));
     }
 
