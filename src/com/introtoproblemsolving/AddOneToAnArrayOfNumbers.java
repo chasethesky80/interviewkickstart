603595fa78509a35ae468d2a6b7e4d57fa07798e
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-14 14:49:49 +0530

    Implemented add one to array of numbers

--- src/com/introtoproblemsolving/AddOneToAnArrayOfNumbers.java
+++ src/com/introtoproblemsolving/AddOneToAnArrayOfNumbers.java
@@ -1,21 +1,25 @@
 package com.introtoproblemsolving;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class AddOneToAnArrayOfNumbers {
 
     public static void main(String[] args) {
-        System.out.println("ADD ONE "+ addOne(Arrays.asList(1, 2, 3)));
-        System.out.println("ADD ONE "+ addOne(Arrays.asList(9, 9, 9)));
+        System.out.println("ADD ONE "+ addOne(new ArrayList<>(List.of(1, 2, 3))));
+        System.out.println("ADD ONE "+ addOne(new ArrayList<>(List.of(9, 9, 9))));
     }
 
-    private static Integer addOne(final List<Integer> input) {
-        double sum = 0;
-        int N = input.size();
-        for (int i = 0; i < N; i++) {
-            sum = sum + (input.get(i) * Math.pow(10, N - (i+1)));
+    private static List<Integer> addOne(final List<Integer> input) {
+        for (int i = input.size() - 1; i >= 0; i--) {
+            input.set(i, input.get(i) + 1);
+            if (input.get(i).compareTo(10) < 0) {
+                return input;
+            }
+            input.set(i, 0);
         }
-        return (int)(sum + 1);
+        input.add(0, 1);
+        return input;
     }
 }
