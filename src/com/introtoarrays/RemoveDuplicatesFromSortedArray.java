46946587f9ee53597408ad151854a25a3542e9e3
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-22 01:23:26 +0530

    Implemented remove duplicates from sorted array

---
+++ src/com/introtoarrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,26 @@
+package com.introtoarrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given a sorted array with duplicates remove duplicates and return index after removing all duplicates
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+        System.out.println("REMOVE DUPLICATES FROM SORTED ARRAY "+ removeDuplicates(Arrays.asList(0, 1, 2, 2, 2, 2, 3, 3, 4)));
+    }
+
+    private static Integer removeDuplicates(final List<Integer> input) {
+        int i = 0;
+        for (int j = 1; j < input.size(); j++){
+            while (input.get(i).compareTo(input.get(j)) == 0) {
+                j++;
+            }
+            i++;
+            input.set(i, input.get(j));
+        }
+        return i+1;
+    }
+}
