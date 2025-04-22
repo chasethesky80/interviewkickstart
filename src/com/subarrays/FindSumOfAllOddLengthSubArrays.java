9c752ee180abfd1e9e79bd5f4a722cc42d253ff8
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-23 00:33:56 +0530

    Implemented sum of all odd length subarrays

--- src/com/subarrays/FindSumOfAllOddLengthSubArrays.java
+++ src/com/subarrays/FindSumOfAllOddLengthSubArrays.java
@@ -8,10 +8,10 @@ import java.util.List;
 public class FindSumOfAllOddLengthSubArrays {
 
     public static void main(String[] args) {
-        System.out.println("SUM OF ODD LENGTH SUBARRAYS " + sumOfAllOddLengthSubArrays(Arrays.asList(1, 4, 2, 5, 3)));
+        System.out.println("SUM OF ODD LENGTH SUBARRAYS " + sumOfAllOddLengthSubArraysBruteForce(Arrays.asList(1, 4, 2, 5, 3)));
     }
 
-    private static Integer sumOfAllOddLengthSubArrays(final List<Integer> input) {
+    private static Integer sumOfAllOddLengthSubArraysBruteForce(final List<Integer> input) {
         final List<Integer> prefixSumArray = ArrayUtils.buildPrefixSumArray(input);
         int sum = 0;
         for (int i = 0; i < input.size(); i++) {
