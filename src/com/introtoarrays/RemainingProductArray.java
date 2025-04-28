dafa15d1cc289bb0de3a03b5c82f0c670f0a1866
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-28 15:38:01 +0530

    Implemented alternate solution for remaning product array

--- src/com/introtoarrays/RemainingProductArray.java
+++ src/com/introtoarrays/RemainingProductArray.java
@@ -11,6 +11,7 @@ public class RemainingProductArray {
 
     public static void main(String[] args) {
         System.out.println("REMANING PROODUCT ARRAY FOR "+ remainingElementsProductArray(Arrays.asList(1, 2, 3, 4, 5)));
+        System.out.println("REMANING PROODUCT ARRAY FOR "+ remainingElementsProductArrayAlternate(Arrays.asList(1, 2, 3, 4, 5)));
     }
 
     private static List<Integer> remainingElementsProductArray(final List<Integer> input) {
@@ -27,6 +28,25 @@ public class RemainingProductArray {
         for (int i = input.size()-2; i>= 0; i--) {
             ans = ans * input.get(i+1);
             result.set(i, result.get(i) * ans);
+        }
+        return result;
+    }
+
+    private static List<Integer> remainingElementsProductArrayAlternate(final List<Integer> input) {
+        int leftProduct = 1;
+        final List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            result.add(1);
+        }
+        for (int i = 0; i < input.size(); i++) {
+            result.set(i, leftProduct);
+            leftProduct = leftProduct * input.get(i);
+        }
+
+        int rightProduct = 1;
+        for (int j = input.size() - 1; j >= 0; j--) {
+            result.set(j, result.get(j) * rightProduct);
+            rightProduct = rightProduct * input.get(j);
         }
         return result;
     }
