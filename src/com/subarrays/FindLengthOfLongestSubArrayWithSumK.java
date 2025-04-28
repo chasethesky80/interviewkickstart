eff1dcb3ee39b8932da44f55463c939a9fc1d8b3
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-28 19:08:24 +0530

    Made small change in implementation

--- src/com/subarrays/FindLengthOfLongestSubArrayWithSumK.java
+++ src/com/subarrays/FindLengthOfLongestSubArrayWithSumK.java
@@ -40,8 +40,7 @@ public class FindLengthOfLongestSubArrayWithSumK {
      * 2 POINTER APPROACH IF THE SUBARRAY ONLY CONTAINS POSITIVES AND ZEROS
      */
     private static Integer longestSubArrayWithSumKTwoPointerApproach(final List<Integer> input, final Integer K) {
-        int i = 0, j = 0, sum = 0, maxLength = Integer.MIN_VALUE;
-        sum = sum + input.get(0);
+        int i = 0, j = 0, sum = input.get(0), maxLength = Integer.MIN_VALUE;
         while (j < input.size()) {
             if (sum == K) {
                 maxLength = Math.max(maxLength, j-i + 1);
