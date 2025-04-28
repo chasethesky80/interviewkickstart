95fba6c03274affbc0f00b5395ad52b4d58c0f3e
chasethesky79 <seshadri.bharath@yahoo.com>
2025-04-28 15:12:19 +0530

    Implemented functionality to check if subarray with target sum exists

---
+++ src/com/subarrays/CheckIfExistsSubArrayOfLengthKWithSum.java
@@ -0,0 +1,30 @@
+package com.subarrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CheckIfExistsSubArrayOfLengthKWithSum {
+
+    public static void main(String[] args) {
+        System.out.println("CHECK IF SUBARRAY EXISTS WITH SIZE K AND TARGET SUM "+ checkIfSubarrayExists(Arrays.asList(2, 3, 1, 2, 4, 3), 3,
+                7));
+    }
+
+    private static boolean checkIfSubarrayExists(final List<Integer> input, final int K, final int targetSum) {
+        int sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum = sum + input.get(i);
+        }
+        if (sum == targetSum) {
+            return true;
+        }
+        for (int i = 1; i <= input.size() - K; i++) {
+            int j = K + i - 1;
+            sum = sum - input.get(i-1) + input.get(j);
+            if (sum == targetSum) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
