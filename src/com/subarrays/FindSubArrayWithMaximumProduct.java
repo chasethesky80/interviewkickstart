22d0b94e374652211b95376f7602518403fca604
chasethesky79 <seshadri.bharath@yahoo.com>
2025-05-14 16:09:37 +0530

    Added another test case for max product subarray

--- src/com/subarrays/FindSubArrayWithMaximumProduct.java
+++ src/com/subarrays/FindSubArrayWithMaximumProduct.java
@@ -4,6 +4,8 @@ public class FindSubArrayWithMaximumProduct {
 
     public static void main(String[] args) {
         System.out.println("FIND SUBARRAY WITH MAX PRODUCT "+ findSubArrayWithMaximumProduct(new int[]{ 2, 3, -2, 4 }));
+        System.out.println("FIND SUBARRAY WITH MAX PRODUCT "+ findSubArrayWithMaximumProduct(new int[]{ 2, 3, 1, 4 }));
+        System.out.println("FIND SUBARRAY WITH MAX PRODUCT "+ findSubArrayWithMaximumProduct(new int[]{ 2, 3, 0, 4 }));
         System.out.println("FIND SUBARRAY WITH MAX PRODUCT "+ findSubArrayWithMaximumProduct(new int[]{ 2, 3, -2, -5 }));
         System.out.println("FIND SUBARRAY WITH MAX PRODUCT "+ findSubArrayWithMaximumProduct(new int[]{ 2, 3, -2, 4, -5 }));
         System.out.println("FIND SUBARRAY WITH MAX PRODUCT "+ findSubArrayWithMaximumProduct(new int[]{ -2, 0, -1 }));
