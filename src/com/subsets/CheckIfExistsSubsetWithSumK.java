2a633cf328497da619a1353d3615e5a19609b2c8
chasethesky79 <seshadri.bharath@yahoo.com>
2025-05-20 16:41:02 +0530

    DSA problems

---
+++ src/com/subsets/CheckIfExistsSubsetWithSumK.java
@@ -0,0 +1,39 @@
+package com.subsets;
+
+import com.util.BitManipulationHelper;
+
+/**
+ * GIVEN AN ARRAY OF SIZE N WITH DISTINCT ELEMENTS, CHECK IF THERE EXISTS A SUBSET OF SUM K
+ */
+public class CheckIfExistsSubsetWithSumK {
+
+    public static void main(String[] args) {
+        System.out.println("DOES EXIST SUBSET WITH SUM K "+ doesExistSubsetOfSumK(new int[]{ 3, -1, 0, 6, 2, -3, 5}, 10));
+    }
+
+    /**
+     * Using bit manipulation since an array with size N can produce 2^N subsets
+     * @param arr
+     * @param k
+     * @return
+     */
+    private static boolean doesExistSubsetOfSumK(int[] arr, int k) {
+        int N = arr.length;
+        for (int i = 0; i < Math.pow(2, N); i++) {
+            /**
+             * HERE WE ARE COMPUTING THE SUM OF ALL NUMBERS IN THE ARRAY OF SIZE N
+             * THAT HAS THE BIT J SET IN WHICH CASE ADD arr[j] TO THE SUM
+             */
+            int sum = 0;
+            for (int j = 0; j < N; j++) {
+                if (BitManipulationHelper.checkIfBitIsSetInPosition(i, j)) {
+                    sum += arr[j];
+                }
+            }
+            if (sum == k) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
