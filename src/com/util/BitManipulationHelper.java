85d00ff2d626ad11bc5bfa286a9f40390a57c131
chasethesky79 <seshadri.bharath@yahoo.com>
2025-05-21 16:07:47 +0530

    Implemented conversion from decimal to binary and vice versa

--- src/com/util/BitManipulationHelper.java
+++ src/com/util/BitManipulationHelper.java
@@ -1,8 +1,16 @@
 package com.util;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class BitManipulationHelper {
+
+    public static void main(String[] args) {
+        System.out.println("BINARY REPRESENTATION OF 13 "+ Arrays.toString(convertToBinary(13, 8)));
+        System.out.println("DECIMAL EQUIVALENT FOR 13 IN BINARY "+ binaryToDecimal(Arrays.asList(1, 1, 0, 1)));
+        System.out.println("BINARY REPRESENTATION OF 13 "+ Arrays.toString(convertToBinary(17, 8)));
+        System.out.println("DECIMAL EQUIVALENT FOR 17 IN BINARY "+ binaryToDecimal(Arrays.asList(1, 0, 0, 0, 1)));
+    }
 
     // TC = O(1) and SC = O(1) since we are just using right shift operator and & operator for solution
     public static boolean checkIfBitIsSetInPosition(int a, int i) {
@@ -20,7 +28,7 @@ public class BitManipulationHelper {
             binary[index++] = N % 2;
             N = N/2;
         }
-        return binary;
+        return ArrayUtils.reversePartOfArray(binary, 0, binary.length - 1);
     }
 
     public static int binaryToDecimal(List<Integer> binaryArray) {
@@ -31,7 +39,7 @@ public class BitManipulationHelper {
             if (binaryArray.get(i) != 0 && binaryArray.get(i) != 1) {
                 throw new IllegalArgumentException("Invalid binary array. It should contain only 0s and 1s.");
             }
-            decimal += binaryArray.get(i) * Math.pow(2, power);
+            decimal += (int) (binaryArray.get(i) * Math.pow(2, power));
             power++;
         }
 
