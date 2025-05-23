db3bda0541806ac6810103c81eb9a3e9afbfbf2f
chasethesky79 <seshadri.bharath@yahoo.com>
2025-05-23 18:23:25 +0530

    Implemented sum of max of all subsequences

---
+++ src/com/subsets/FindSumOfMaxOfAllSubsequences.java
@@ -0,0 +1,35 @@
+package com.subsets;
+
+import com.util.BitManipulationHelper;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * Given an array find the sum of max of all subsequences of the array
+ */
+public class FindSumOfMaxOfAllSubsequences {
+
+    public static void main(String[] args) {
+        System.out.println("SUM OF MAX OF ALL SUBSEQUENCES OF ARRAY "+ findSumOfMaxOfAllSubsequences(new int[]{ 3, 1, -4 }));
+    }
+
+    private static int findSumOfMaxOfAllSubsequences(int[] arr) {
+        int N = arr.length, sum = 0;
+        for (int i = 0; i < Math.pow(2, N); i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = 0; j < N; j++) {
+                if (BitManipulationHelper.checkIfBitIsSetInPosition(i, j)) {
+                    max = Math.max(max, arr[j]);
+                }
+            }
+            /**
+             * Accommodate for the edge case where there can be an empty subset in which case max is never reassigned
+             * and so compute sum only when max is assigned a valid value other than Integer.MIN_VALUE
+             */
+            if (max > Integer.MIN_VALUE) {
+                sum += max;
+            }
+        }
+        return sum;
+    }
+}
