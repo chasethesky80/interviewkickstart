00f9c53cab54fb8d2bbcbef3b439a14f64feeb47
chasethesky79 <seshadri.bharath@yahoo.com>
2025-06-23 21:38:54 +0530

    Search sorted array binary search

--- src/com/searching/ImplementBinarySearchOnASortedArray.java
+++ src/com/searching/ImplementBinarySearchOnASortedArray.java
@@ -1,18 +1,23 @@
 package com.searching;
 
-/**
- * GIVEN A SORTED ARRAY PERFORM A BINARY SEARCH TO RETURN INDEX OF TARGET ELEMENT TO BE SEARCHED, IF ELEMENT
- * IS NOT FOUND IN THE ARRAY RETURN -1
+/** LEETCODE - Search Insert Position - REFER TO Searching 1 & Searching 2 videos from Bosscoderacademy for solution
+ Given a sorted array of distinct integers and a target value, return the index if the target is found. If not,
+ return the index where it would be if it were inserted in order.
+ You must write an algorithm with O(log n) runtime complexity.
  */
 public class ImplementBinarySearchOnASortedArray {
 
     public static void main(String[] args) {
-        System.out.println("PERFORMED BINARY SEARCH ON ASORTED ARRAY "+ binarySearch(new int[]{ 2, 3, 5, 6, 7 }, 5));
-        System.out.println("PERFORMED BINARY SEARCH ON ASORTED ARRAY "+ binarySearch(new int[]{ 2, 3, 5, 6, 7 }, 13));
+        System.out.println("PERFORMED BINARY SEARCH ON ASORTED ARRAY "+ binarySearch(new int[]{ 1, 3, 5, 6 }, 5));
+        System.out.println("PERFORMED BINARY SEARCH ON ASORTED ARRAY "+ binarySearch(new int[]{ 1, 3, 5, 6 }, 2));
+        System.out.println("PERFORMED BINARY SEARCH ON ASORTED ARRAY "+ binarySearch(new int[]{ 1, 3, 5, 6 }, 7));
     }
 
     private static int binarySearch(int[] arr, int target) {
         int start = 0, end = arr.length - 1;
+        if (target > arr[end]) {
+            return end + 1;
+        }
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (arr[mid] == target) {
@@ -23,6 +28,6 @@ public class ImplementBinarySearchOnASortedArray {
                 end = mid - 1;
             }
         }
-        return -1;
+        return start;
     }
 }
