706ea3d8702d024481fe2afe6ceb486e84e7caa3
chasethesky79 <seshadri.bharath@yahoo.com>
2025-07-13 13:50:08 +0530

    Implemented peak element using binary search

--- src/com/binarysearch/FindPeakElementInAnArray.java
+++ src/com/binarysearch/FindPeakElementInAnArray.java
@@ -15,6 +15,7 @@ public class FindPeakElementInAnArray {
         System.out.println("Find peak element in an array "+ findPeakElement(new int[]{ 1, 2, 3, 1 }));
         System.out.println("Find peak element in an array "+ findPeakElement(new int[]{ 1, 2, 1, 3, 5, 6, 4 }));
         System.out.println("Find peak element in an array "+ findPeakElement(new int[]{ 1, 4, 2, 3, 5, 6, 4 }));
+        System.out.println("Find peak element in an array "+ findPeakElement(new int[]{ 1, 5, 1, 2, 1 }));
     }
     private static int findPeakElement(int[] nums) {
         if (nums.length == 1 || nums[0] > nums[1]) {
