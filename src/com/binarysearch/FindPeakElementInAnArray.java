01a525f725f146d257e6d6167ab1e094c92b80fe
chasethesky79 <seshadri.bharath@yahoo.com>
2025-07-13 14:04:41 +0530

    Implemented peak element using binary search

--- src/com/binarysearch/FindPeakElementInAnArray.java
+++ src/com/binarysearch/FindPeakElementInAnArray.java
@@ -27,13 +27,12 @@ public class FindPeakElementInAnArray {
         int low = 1, high = nums.length - 2;
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (nums[mid-1] < nums[mid]) {
-                if (nums[mid] > nums[mid + 1]) {
-                    return mid;
-                }
+            if (nums[mid-1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid-1] < nums[mid]) { // INCREASING CURVE - PEAK ON RIGHT
                 low = mid + 1;
             } else {
-                high = mid - 1;
+                high = mid - 1; // ELSE PEAK ON LEFT
             }
         }
         return low;
