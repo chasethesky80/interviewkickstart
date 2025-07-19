87ee50d56ef02fae616a86248c030533c5da44b6
chasethesky79 <seshadri.bharath@yahoo.com>
2025-07-19 16:17:20 +0530

    Implement string combinations with recursion

--- src/com/recursion/GenerateAllBinaryStringsOfLengthN.java
+++ src/com/recursion/GenerateAllBinaryStringsOfLengthN.java
@@ -7,9 +7,12 @@ public class GenerateAllBinaryStringsOfLengthN {
      * REFER BOSSCODER ACADEMY VIDEO: https://platform.bosscoderacademy.com/course/SjP8t8VfLx8zGa8wbGaU
      * @param args
      */
+    private static String s = "";
     public static void main(String[] args) {
         System.out.println("Generate All Binary Strings of Length N ");
         generate("", 3);
+        System.out.println("Generate All Binary Strings of Length N alternative approach");
+        generateAlternative(3);
     }
     private static void generate(String str, int n) {
         if (str.length() == n) {
@@ -18,5 +21,16 @@ public class GenerateAllBinaryStringsOfLengthN {
         }
         generate(str + "0", n);
         generate(str + "1", n);
+    }
+    private static void generateAlternative(int n) {
+        if (s.length() == n) {
+            return;
+        }
+        s=s+"0";
+        generateAlternative(n);
+        s = s.substring(0,s.length()-1);
+        s=s+"1";
+        generateAlternative(n);
+        s = s.substring(0,s.length()-1);
     }
 }
