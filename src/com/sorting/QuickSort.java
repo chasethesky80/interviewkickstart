d255e1c03e53d192a34a9c8e1d4d4edfefecb75a
chasethesky79 <seshadri.bharath@yahoo.com>
2025-07-27 17:53:12 +0530

    Implemented quick sort with lomottos partition

---
+++ src/com/sorting/QuickSort.java
@@ -0,0 +1,41 @@
+package com.sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        System.out.println("QUICK SORT");
+        int[] arr = new int[]{ 7, 3, 4 ,2 ,1, 6 };
+        quickSort(arr);
+        System.out.println("SORTED ARRAY" + Arrays.toString(arr));
+    }
+    private static void quickSort(int[] arr) {
+        quickSortHelper(arr, 0, arr.length - 1);
+    }
+    /**
+     * NOTE: Refer interviewkickstart video lomotto partition on quick sort
+     * @param array
+     * @param start
+     * @param end
+     */
+    private static void quickSortHelper(int[] array, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int smaller = start;
+        for (int bigger = start+1; bigger <= end; bigger++) {
+            if (array[bigger] <= array[start]) {
+                smaller++;
+                int temp = array[smaller];
+                array[smaller] = array[bigger];
+                array[bigger] = temp;
+            }
+        }
+        int temp = array[start];
+        array[start] = array[smaller];
+        array[smaller] = temp;
+        quickSortHelper(array, start, smaller-1);
+        quickSortHelper(array, smaller+1, end);
+    }
+}
