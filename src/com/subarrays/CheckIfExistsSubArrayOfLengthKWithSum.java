886696be7ddebdd782182533b7ce35fd438a7f48
chasethesky79 <seshadri.bharath@yahoo.com>
2026-08-16 15:17:56 +0530

    Implemented alternative approach to sliding window

--- src/com/subarrays/CheckIfExistsSubArrayOfLengthKWithSum.java
+++ src/com/subarrays/CheckIfExistsSubArrayOfLengthKWithSum.java
@@ -8,6 +8,7 @@ public class CheckIfExistsSubArrayOfLengthKWithSum {
     public static void main(String[] args) {
         System.out.println("CHECK IF SUBARRAY EXISTS WITH SIZE K AND TARGET SUM "+ checkIfSubarrayExists(Arrays.asList(2, 3, 1, 2, 4, 3), 3,
                 7));
+        System.out.println("CHECK IF SUBARRAY EXISTS WITH SIZE K AND TARGET SUM "+ checkIfSubarrayExistsAlternativeApproach(Arrays.asList(2, 3, 1, 2, 4, 3), 3, 7));
     }
 
     private static boolean checkIfSubarrayExists(final List<Integer> input, final int K, final int targetSum) {
@@ -24,6 +25,26 @@ public class CheckIfExistsSubArrayOfLengthKWithSum {
             if (sum == targetSum) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    private static boolean checkIfSubarrayExistsAlternativeApproach(final List<Integer> input, final int K, final int targetSum) {
+        int sum = 0, i = 0;
+        for (; i < K; i++) {
+            sum = sum + input.get(i);
+        }
+        if (sum == targetSum) {
+            return true;
+        }
+        int st = 0;
+        while (i < input.size()) {
+            sum = sum + input.get(i) - input.get(st);
+            if (sum == targetSum) {
+                return true;
+            }
+            i++;
+            st++;
         }
         return false;
     }
