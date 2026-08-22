3e2b94433091762ed8f251911d1a95c48ca28059
chasethesky79 <seshadri.bharath@yahoo.com>
2026-08-22 15:41:28 +0530

    Implemented alternative soln for subarray sum

--- src/com/subarrays/FindNoOfSubarraysOfSizeKWithTargetSum.java
+++ src/com/subarrays/FindNoOfSubarraysOfSizeKWithTargetSum.java
@@ -7,7 +7,9 @@ public class FindNoOfSubarraysOfSizeKWithTargetSum {
 
     public static void main(String[] args) {
         System.out.println("FIND NUMBER OF SUBARRAYS WITH SUM OF "+getNoOfSubarraysOfSizeKWithTargetSum(Arrays.asList(3, 7, 5, 8, 2, 10), 3, 15));
+        System.out.println("FIND NUMBER OF SUBARRAYS WITH SUM OF "+getNoOfSubarraysOfSizeKWithTargetSumAlternative(Arrays.asList(3, 7, 5, 8, 2, 10), 3, 15));
         System.out.println("FIND NUMBER OF SUBARRAYS WITH SUM OF "+getNoOfSubarraysOfSizeKWithTargetSum(Arrays.asList(1, 2, 3, 2, 3, 4), 2, 5));
+        System.out.println("FIND NUMBER OF SUBARRAYS WITH SUM OF "+getNoOfSubarraysOfSizeKWithTargetSumAlternative(Arrays.asList(1, 2, 3, 2, 3, 4), 2, 5));
     }
 
     private static int getNoOfSubarraysOfSizeKWithTargetSum(final List<Integer> input, final int K, final int targetSum) {
@@ -24,6 +26,26 @@ public class FindNoOfSubarraysOfSizeKWithTargetSum {
             if (sum == targetSum) {
                 count++;
             }
+        }
+        return count;
+    }
+
+    private static int getNoOfSubarraysOfSizeKWithTargetSumAlternative(final List<Integer> input, final int K, final int targetSum) {
+        int st = 0, end = 0, count = 0, sum = 0;
+        for (end = 0; end < K; end++) {
+            sum = sum + input.get(end);
+        }
+        if (sum == targetSum) {
+            count++;
+        }
+        while (end < input.size()) {
+            sum = sum + input.get(end);
+            sum = sum - input.get(st);
+            if (sum == targetSum) {
+                count++;
+            }
+            end++;
+            st++;
         }
         return count;
     }
